117a0b22940a24fedb7400cd03be045fd99fc262
Oxyless <c.bruchon@wizville.fr>
2021-12-15 16:12:04 +0100

    Push docs

---
+++ docs/_nuxt/static/1639581102/tunes/Las Vegas Tango/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FProjetMusical\u002F_nuxt\u002Fstatic\u002F1639581102",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftunes\u002FLas%20Vegas%20Tango",config:{_app:{basePath:"\u002FProjetMusical\u002F",assetsPath:"\u002FProjetMusical\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e5dbdbe3"}}}}(null));
