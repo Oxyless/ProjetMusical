0a2975a7d0cbf31e05e5ea0c74fca42c2572d824
Oxyless <c.bruchon@wizville.fr>
2021-12-15 16:08:55 +0100

    Remove dist

--- dist/_nuxt/static/1639580854/tunes/Watermelon Man/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FProjetMusical\u002F_nuxt\u002Fstatic\u002F1639580854",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftunes\u002FWatermelon%20Man",config:{_app:{basePath:"\u002FProjetMusical\u002F",assetsPath:"\u002FProjetMusical\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db7c16b7"}}}}(null));
