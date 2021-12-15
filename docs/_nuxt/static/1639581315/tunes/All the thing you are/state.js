a281bf1d5c2562a0e86a4096f6979469f99ebaf1
Oxyless <c.bruchon@wizville.fr>
2021-12-15 20:29:52 +0100

    Deploy

--- docs/_nuxt/static/1639581315/tunes/All the thing you are/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FProjetMusical\u002F_nuxt\u002Fstatic\u002F1639581315",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftunes\u002FAll%20the%20thing%20you%20are",config:{_app:{basePath:"\u002FProjetMusical\u002F",assetsPath:"\u002FProjetMusical\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0d2e03e2"}}}}(null));
