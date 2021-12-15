5b1436d0043364708c874e75367ee5306ae42810
Oxyless <c.bruchon@wizville.fr>
2021-12-15 16:15:23 +0100

    Publish

--- docs/_nuxt/static/1639581207/tunes/Blues Walk/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639581207",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftunes\u002FBlues%20Walk",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e40c617d"}}}}(null));
---
+++ docs/_nuxt/static/1639581315/tunes/Red Clay/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FProjetMusical\u002F_nuxt\u002Fstatic\u002F1639581315",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftunes\u002FRed%20Clay",config:{_app:{basePath:"\u002FProjetMusical\u002F",assetsPath:"\u002FProjetMusical\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0d2e03e2"}}}}(null));
