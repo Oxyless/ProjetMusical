c85d808f278313e37df75588af21f2d067867c59
Oxyless <c.bruchon@wizville.fr>
2021-12-16 17:30:43 +0100

    Publish

---
+++ pages/tunes/_uid.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row justify="center">
+    <v-col>
+      <client-only>
+        <Tune :tuneProfile="tuneProfile" v-if="tuneProfile" />
+      </client-only>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "tune",
+  data() {
+    return {
+      tuneProfile: null
+    }
+  },
+  async fetch() {
+    this.tuneProfile = (await this.$content('tunes').where({ 
+      uid: this.$route.params.uid 
+    }).fetch())
+  }
+}
+</script>
+
+<style>
+  #paper {
+    background-color: white;
+    width: 21cm;
+  }
+</style>
