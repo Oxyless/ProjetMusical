ce8612bcaf82388d4dd21685dd5796c84a52a795
Oxyless <c.bruchon@wizville.fr>
2021-12-16 16:22:43 +0100

    Publish

---
+++ docs/_nuxt/static/1639668150/tunes/Majhong/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FProjetMusical\u002F_nuxt\u002Fstatic\u002F1639668150",layout:"default",error:a,state:{options:{print:false}},serverRendered:true,routePath:"\u002Ftunes\u002FMajhong",config:{_app:{basePath:"\u002FProjetMusical\u002F",assetsPath:"\u002FProjetMusical\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a8255a00"}}}}(null));
